b02ab445c28fc653d314b82b05ffcc06757d4690
Mrs Qu <862792055@qq.com>
2019-09-11 16:20:46 +0800

    ss

--- js/index.js
+++ js/index.js
@@ -1,9 +1,22 @@
-var swiper = new Swiper('.swiper-container', {
+ new Swiper('.swiper-container1', {
+	 autoplay:{
+		 stopOnLastSlide:true
+	 },
+	pagination: {
+		el: '.swiper-pagination1',
+		clickable: true,
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
+var swiper = new Swiper('.swiper-container2', {
 	slidesPerView: 4,
 	spaceBetween: 5,
 	freeMode: true,
 	pagination: {
-		el: '.swiper-pagination',
+		el: '.swiper-pagination2',
 		clickable: true,
 	},
 	navigation: {
